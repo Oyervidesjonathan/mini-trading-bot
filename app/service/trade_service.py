41bcc7cec0afce5acd09918e25c9c7c1fb319f05
Jonathan Oyervides <oyervidesjonathan@yahoo.com>
2026-03-14 14:58:42 -0500

    add enviorment configuration loader

--- app/service/trade_service.py
+++ app/service/trade_service.py
@@ -7,6 +7,24 @@ def execute_trade(symbol: str, quantity: int):
     
     price = get_price(symbol)
 
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO trades (symbol, quantity, price, timestamp)
+        VALUES(%s, %s, %s, %s)
+        RETURNING id
+        """,
+        (symbol.upper(), quantity, price, datetime.utcnow())
+    )
+
+    trade_id = cursor.fetchone()[0]
+
+    con.commit()
+    cursor.close()
+    conn.close()
+
     trade = {
         "symbol": symbol.upper(),
         "quantinty": quantity,
