931f860e8a592741476499aa55dbb7981f152afa
Jonathan Oyervides <oyervidesjonathan@yahoo.com>
2026-03-15 18:11:52 -0500

    docs: add docstrings to trade data model

--- app/db/models.py
+++ app/db/models.py
@@ -1,12 +1,30 @@
 
+"""
+Trade Data Model
+
+Defines the structure used to represent a trade within 
+the Mini Trading Bot application. This model is used 
+to organize trade data retrived from or stored in the database. 
+"""
 from dataclasses import dataclass
 from datetime import datetime
 
 @dataclass
 class Trade:
+    """
+    Represents a single execution trade.
+
+    Attributes:
+        symbol (str): Stock ticker symbol(e.g., AAPL, TSLA).
+        quantity (int): Number off shares traded.
+        price (float): Price per share at the time of the trade.
+        timestamp (datetime): Time whe the trade occured.
+        status (str): Current trade status (e.g., FILLED, PENDING).
+
+    """
     symbol: str
     quantity:int
     price: float
-    timsestamp: datetime
+    timestamp: datetime
     status: str
 
