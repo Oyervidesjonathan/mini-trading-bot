19c1b888fe2adc867cc0c3e4cfa342809e38704b
Jonathan Oyervides <oyervidesjonathan@yahoo.com>
2026-03-15 17:57:43 -0500

    docs: add docstring to datbase utility functions

--- app/db/db.py
+++ app/db/db.py
@@ -1,13 +1,34 @@
 
+"""
+Database Utility Module
+
+Provides helper functions for creating database connections 
+and initializing the database schema for the Mini Trading Bot.
+"""
 import psycopg
 import os
 
 from app.config import DATABASE_URL
 
 def get_connection():
+    """
+    Create and return a PostgreSQL database connection.
+
+        Returns:
+            psycopg.Connection: Acitve connection to the database
+    """
     return psycopg.connect(DATABASE_URL)
 
 def init_db():
+    """
+    Initializes the database schema.
+
+    This function reads the sql schema file and executes it
+    to create the required tables id they do not already exist.
+
+    Returns:
+        None
+    """
     conn = get_connection()
     cursor = conn.cursor()
 
