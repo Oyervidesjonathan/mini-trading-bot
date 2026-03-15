78d95f8695642bb6d18d0ac7a2365f86499b5cab
Jonathan Oyervides <oyervidesjonathan@yahoo.com>
2026-03-15 13:08:37 -0500

    feat: add bot runner endpoint to execute strategy pipeline

--- app/api/run.py
+++ app/api/run.py
@@ -10,3 +10,27 @@ def run_startegy(symbol: str):
     result = evaluate_strategy(symbol)
 
     return result
+
+@router.post("/bot/run/{symbol}")
+def run_bot(symbol: str, quantity: int = 1):
+
+    strategy_result = evaluate_strategy(symbol)
+
+    if strategy_result["decision"] == "BUY":
+        trade_results = execute_trade(symbol, quantity)
+
+        return {
+            "symbol": strategy_result["symbol"],
+            "price": strategy_result["price"],
+            "decision": strategy_result["decision"],
+            "trade_executed": True,
+            "trade": trade_result
+        }
+
+    return{
+        "symbol": strategy_result["symbol"],
+        "price": strategy_result["price"],
+        "decision": strategy_result["decision"],
+        "trade_executed": False,
+        "trade": None
+    }
