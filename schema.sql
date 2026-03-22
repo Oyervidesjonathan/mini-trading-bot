c88df3a14449658595038769cf1b56463d31b404
Jonathan Oyervides <Oyervidesjonathan@Gmail.com>
2026-03-22 13:51:44 -0500

    added users, portafolios, and positions tables

--- schema.sql
+++ schema.sql
@@ -9,3 +9,29 @@ CREATE TABLE IF NOT EXISTS trades (
     status TEXT NOT NULL
     
 );
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password, TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
+CREATE TABLE IF NOT EXISTS portafolios (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id),
+  balance NUMERIC DEFAULT 10000,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  
+);
+
+CREATE TABLE IF NOT EXISTS positions (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES user(id),
+  symbol TEXT NOT NULL,
+  quantity INTEGER NOT NULL,
+  avg_price NUMERIC DEFAULT 0
+
+);
+
